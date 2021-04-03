ab6ffc9d21ddc4dda79b02d184c3167ed421f782
Mastewal B <se.mastewal.belete@gmail.com>
2021-04-03 19:40:32 +0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import RowComponent from './RowComponent';
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Nav />
+      <Banner />
+      <RowComponent title="Netflix Originals" fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow />
+      <RowComponent title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <RowComponent title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <RowComponent title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <RowComponent title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <RowComponent title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <RowComponent title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <RowComponent title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
     </div>
   );
 }
